67f931b5e40746d00c706c5b57635f60b2ab2be2
ByAlexShark <alexanderzafiro01@gmail.com>
2025-09-05 10:30:50 -0400

    Saludar en español e inglés

--- src/script.js
+++ src/script.js
@@ -1,24 +1,25 @@
 document.getElementById('saludarBtn').addEventListener('click', function() {
   const nombre = document.getElementById('nombre').value;
   const genero = document.getElementById('genero').value;
+  const idioma = document.getElementById('idioma').value;
 
   const fechaActual = new Date();
   const horaActual = fechaActual.getHours();
 
   let saludoHora = '';
   if (horaActual < 12) {
-    saludoHora = 'Buenos días';
+    saludoHora = idioma === 'espanol' ? 'Buenos días' : 'Good morning';
   } else if (horaActual < 19) {
-    saludoHora = 'Buenas tardes';
+    saludoHora = idioma === 'espanol' ? 'Buenas tardes' : 'Good afternoon';
   } else {
-    saludoHora = 'Buenas noches';
+    saludoHora = idioma === 'espanol' ? 'Buenas noches' : 'Good evening';
   }
 
   let saludoGenero = '';
   if (genero === 'masculino') {
-    saludoGenero = 'Sr.';
+    saludoGenero = idioma === 'espanol' ? 'Sr.' : 'Mr.';
   } else if (genero === 'femenino') {
-    saludoGenero = 'Sra.';
+    saludoGenero = idioma === 'espanol' ? 'Sra.' : 'Mrs.';
   }
 
   if (nombre) {
