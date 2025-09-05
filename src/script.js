219b70480d2ff0fbc9c2d1eb82b3cc69db643e9f
ByAlexShark <alexanderzafiro01@gmail.com>
2025-09-05 10:24:33 -0400

    Saludar de acuerdo a la hora

--- src/script.js
+++ src/script.js
@@ -1,9 +1,24 @@
 document.getElementById('saludarBtn').addEventListener('click', function() {
+  // Obtenemos el nombre del campo de texto
   const nombre = document.getElementById('nombre').value;
 
-  if (nombre) {
-    document.getElementById('saludo').textContent = `Hola, ${nombre}!`;
+  // Obtener la hora actual
+  const fechaActual = new Date();
+  const horaActual = fechaActual.getHours();
+
+  // Saludar dependiendo de la hora
+  let saludoHora = '';
+  if (horaActual < 12) {
+    saludoHora = 'Buenos días';
+  } else if (horaActual < 19) {
+    saludoHora = 'Buenas tardes';
   } else {
-    document.getElementById('saludo').textContent = 'Hola, ¿cómo te llamas?';
+    saludoHora = 'Buenas noches';
+  }
+
+  if (nombre) {
+    document.getElementById('saludo').textContent = `${saludoHora}, ${nombre}!`;
+  } else {
+    document.getElementById('saludo').textContent = `${saludoHora}, ¿cómo te llamas?`;
   }
 });
